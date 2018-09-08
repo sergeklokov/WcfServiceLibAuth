df0b7f4fb18a198846d006f911c9fb592cb031b2
serge klokov <sergeklokov@users.noreply.github.com>
2018-09-07 21:12:48 -0700

    Initial commit

---
+++ ServiceAuthentificator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IdentityModel.Selectors;
+using System.IdentityModel.Tokens;
+
+namespace WcfServiceLibAuth
+{
+    public class ServiceAuthentificator: UserNamePasswordValidator 
+    {
+        public override void Validate(string userName, string password)
+        {
+            if (string.IsNullOrEmpty(userName))
+                throw new ArgumentNullException("userName");
+
+            if (string.IsNullOrEmpty(password))
+                throw new ArgumentNullException("password");
+
+            if (userName!="test" || password != "test")
+                throw new SecurityTokenException("Unknown username or password");
+
+        }
+    }
+}
